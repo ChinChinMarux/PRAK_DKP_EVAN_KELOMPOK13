c14e89002d55301d1b642f02af7f5c9f8c920093
Evan <evanadkaracp@gmail.com>
2024-03-28 15:20:15 +0700

    Update ToDoList.cpp

--- ToDoList.cpp
+++ ToDoList.cpp
@@ -4,3 +4,28 @@
 #include <vector>
 
 using namespace std;
+
+struct Task
+{
+    string deskripsi;
+    bool selesai;
+    Task(const string &desc, bool status = false) : deskripsi(desc), selesai(status) {}
+};
+
+class TaskManager
+{
+private:
+    vector<Task> tugas;
+
+public:
+    TaskManager()
+    {
+        tugas = loadTasks();
+    }
+
+    vector<Task> loadTasks()
+    {
+        vector<Task> loadTugas;
+        ifstream file("tugas.txt");
+    }
+};
